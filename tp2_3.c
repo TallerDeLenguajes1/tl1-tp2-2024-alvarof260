2c582e9c929d165907cda60054a04e981973b04b
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-04-09 16:51:55 -0300

    feat: agregar operaciones aritmeticas a la funcion de matriz

---
+++ tp2_3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 5
+#define M 7
+
+int main()
+{
+    srand(time(NULL));
+    int i, j;
+    int mt[N][M];
+    int *p;
+    p = mt;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            *p = 1 + rand() % 100;
+            printf("%d\n", *p);
+            p++;
+           
+        }
+        printf("\n");
+    }
+    return 0;
+}
