4c4115d46d46bd7cc0cd657ca644bd6f4182d7ee
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-04-10 00:02:24 -0300

    feat: funcion para mostrar la pc mas rapida

--- tp2_4.c
+++ tp2_4.c
@@ -13,6 +13,7 @@ struct
 void cargarPc(computadora pcs[], int n);
 void mostrarPc(computadora pcs[], int n);
 void mostrarPcMasVieja(computadora pcs[], int n);
+void mostrarPcMasRapida(computadora pcs[], int n);
 
 char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
 
@@ -23,6 +24,7 @@ int main()
     cargarPc(pcs, 5);
     mostrarPc(pcs, 5);
     mostrarPcMasVieja(pcs,5);
+    mostrarPcMasRapida(pcs,5);
     return 0;
 }
 
@@ -62,5 +64,21 @@ void mostrarPcMasVieja(computadora pcs[], int n)
     printf("%d\n", masVieja.anio);
     printf("%d\n", masVieja.cantidad);
     printf("%s\n", masVieja.tipo_cpu);
+    printf("\n");
+}
+
+void mostrarPcMasRapida(computadora pcs[], int n)
+{
+    computadora masRapida = pcs[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (masRapida.velocidad < pcs[i].velocidad)
+            masRapida = pcs[i];
+    }
+    printf("pc mas rapida\n");
+    printf("%d\n", masRapida.velocidad);
+    printf("%d\n", masRapida.anio);
+    printf("%d\n", masRapida.cantidad);
+    printf("%s\n", masRapida.tipo_cpu);
     printf("\n");
 }
